6b51b6b9f443ecb598c02321c89579dd76a8e579
s-razoes <49306008+s-razoes@users.noreply.github.com>
2021-03-20 22:44:07 +0000

    Re-read the assignment and made the changes accordingly

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -5,26 +5,26 @@ angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController );
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
-  $scope.message = "Please enter data first.";
+  $scope.message = "";
   $scope.list_of_items = "";
-  $scope.message_style = "Red";
-  $scope.lunchCheck = function (){
+  $scope.color_style = "Black";
+  $scope.checkIfTooMuch = function (){
+    if ($scope.list_of_items == ""){
+      $scope.message = "Please enter data first!";
+      $scope.color_style = "Red";
+      return;
+    }
     var items = $scope.list_of_items.split(',')
       .filter(function (val){
         return val.trim().length > 0;
       });
     var countOfItems = items.length;
-    if(countOfItems == 3){
+    if(countOfItems <= 3){
       $scope.message = "Enjoy";
-      $scope.message_style = "Green";
-    }else
-      if(countOfItems == 4){
-        $scope.message = "Too Much!";
-        $scope.message_style = "Green";
-      }else{
-        $scope.message = "Sounds like an incorrect list or items!";
-        $scope.message_style = "Red";
-      }
+    }else{
+      $scope.message = "Too Much!";
+    }
+    $scope.color_style = "Green";
   };
 }
 
